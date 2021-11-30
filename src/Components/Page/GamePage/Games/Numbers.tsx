5e71b2e704f7a531b8da1c73bad0ac612037c8ac
Victor Reaboi <victor.reaboi@mediapark.com>
2021-11-30 20:39:08 +0200

    adding numbers game and saving history

--- src/Components/Page/GamePage/Games/Numbers.tsx
+++ src/Components/Page/GamePage/Games/Numbers.tsx
@@ -1,3 +1,61 @@
+import { saveGameHistory } from "../../../../utils/gameData/saveScore";
+
+import { MouseEventHandler } from "react";
+import { GameType } from "../types";
+
 export const Numbers = () => {
-  return <div>numbers game</div>;
+  const numbers = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  ];
+
+  let currentNumber = 1,
+    clickCounter = 0,
+    startTime: number;
+
+  const numberClickHandler: MouseEventHandler<HTMLSpanElement> = (
+    event
+  ): void => {
+    const target = event.target as HTMLElement;
+
+    ++clickCounter;
+
+    if (currentNumber === 1) {
+      startTime = Date.now();
+    }
+    if (currentNumber === 20) {
+      const diffMinutes = Math.ceil(Date.now() - startTime / (1000 * 60));
+
+      saveGameHistory(GameType.numbers, { diffMinutes, clickCounter });
+    }
+
+    if (currentNumber === Number(target.getAttribute("data-value"))) {
+      target.classList.add("clicked");
+      ++currentNumber;
+    }
+  };
+
+  return (
+    <div className="gameScreen">
+      <h1>
+        Numbers game <span title="Informații despre joc">-i-</span>
+      </h1>
+      {numbers.map((number, i) => {
+        return (
+          <span
+            key={i}
+            data-value={number}
+            className="number"
+            onClick={numberClickHandler}
+            style={{
+              position: "absolute",
+              top: Math.round(Math.random() * 90) + "vh",
+              left: Math.round(Math.random() * 90) + "vw",
+            }}
+          >
+            {number}
+          </span>
+        );
+      })}
+    </div>
+  );
 };
---
+++ src/utils/gameData/saveScore.tsx
@@ -0,0 +1,38 @@
+import { GameType } from "../../Components/Page/GamePage/types";
+import { getFromLocalStorage, saveToLocalStorage } from "./localStorage";
+import { GamesData } from "./types";
+
+export const saveGameHistory = (
+  game: GameType,
+  dataToSave: Record<string, unknown>
+) => {
+  const data = getFromLocalStorage("g-data") as GamesData;
+
+  if (!data) {
+    const newData = {
+      games: {
+        [game]: {
+          history: [dataToSave],
+        },
+      },
+    };
+
+    saveToLocalStorage("g-data", newData);
+  } else {
+    const data = getFromLocalStorage("g-data") as GamesData;
+
+    if (data.games[game]) {
+      const newData = {
+        games: {
+          ...data.games,
+
+          [game]: {
+            history: [...data.games[game].history, dataToSave],
+          },
+        },
+      };
+
+      saveToLocalStorage("g-data", newData);
+    }
+  }
+};
