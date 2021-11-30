79dc0126678237b6d6d6719237469a071f2c6a87
Victor Reaboi <victor.reaboi@mediapark.com>
2021-11-30 21:30:53 +0200

    logig for colors game and save score bug solved

--- src/Components/Page/GamePage/Games/Numbers.tsx
+++ src/Components/Page/GamePage/Games/Numbers.tsx
@@ -2,15 +2,23 @@ import { saveGameHistory } from "../../../../utils/gameData/saveScore";
 
 import { MouseEventHandler } from "react";
 import { GameType } from "../types";
+import { useState } from "react";
+
+let clickCounter = 0;
+let startTime: number;
 
 export const Numbers = () => {
-  const numbers = [
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-  ];
+  const [_currentNumber, setCurrentNumber] = useState(1);
 
-  let currentNumber = 1,
-    clickCounter = 0,
-    startTime: number;
+  const [a, setA] = useState([
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  ]);
+
+  const numbers = a.map((item) => ({
+    value: item,
+    top: Math.round(Math.random() * 90) + "vh",
+    left: Math.round(Math.random() * 90) + "vw",
+  }));
 
   const numberClickHandler: MouseEventHandler<HTMLSpanElement> = (
     event
@@ -19,40 +27,44 @@ export const Numbers = () => {
 
     ++clickCounter;
 
-    if (currentNumber === 1) {
+    if (_currentNumber === 1) {
       startTime = Date.now();
     }
-    if (currentNumber === 20) {
+    if (_currentNumber === numbers.length) {
       const diffMinutes = Math.ceil(Date.now() - startTime / (1000 * 60));
 
       saveGameHistory(GameType.numbers, { diffMinutes, clickCounter });
+
+      // TODO de aratat un mesaj si statistica din jocul curent
     }
 
-    if (currentNumber === Number(target.getAttribute("data-value"))) {
-      target.classList.add("clicked");
-      ++currentNumber;
+    if (_currentNumber === Number(target.getAttribute("data-value"))) {
+      setCurrentNumber(_currentNumber + 1);
+
+      setA(a.filter((item) => item !== _currentNumber));
     }
   };
 
   return (
     <div className="gameScreen">
       <h1>
-        Numbers game <span title="Informații despre joc">-i-</span>
+        {_currentNumber} Numbers game{" "}
+        <span title="Informații despre joc">-i-</span>
       </h1>
       {numbers.map((number, i) => {
         return (
           <span
             key={i}
-            data-value={number}
+            data-value={number.value}
             className="number"
             onClick={numberClickHandler}
             style={{
               position: "absolute",
-              top: Math.round(Math.random() * 90) + "vh",
-              left: Math.round(Math.random() * 90) + "vw",
+              top: number.top,
+              left: number.left,
             }}
           >
-            {number}
+            {number.value}
           </span>
         );
       })}
