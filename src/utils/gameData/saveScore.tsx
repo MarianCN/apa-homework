02988b517a376b993a9c5c35041e4144211ac5bc
Victor Reaboi <victor.reaboi@mediapark.com>
2021-12-07 19:57:54 +0200

    addint statistic page

--- src/utils/gameData/saveScore.tsx
+++ src/utils/gameData/saveScore.tsx
@@ -16,6 +16,7 @@ export const saveGameHistory = (
         },
       },
     };
+    console.log(newData)
 
     saveToLocalStorage("g-data", newData);
   } else {
@@ -32,6 +33,7 @@ export const saveGameHistory = (
         },
       };
 
+      console.log(newData)
       saveToLocalStorage("g-data", newData);
     }
   }
